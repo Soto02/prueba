b6c2604f5cc63b9b91fde2ab3c11d8db0f211f62
Soto02 <asotfer636@g.educaand.es>
2023-09-22 12:46:43 +0200

    ProgramaPar

---
+++ par/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace par
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa llamado par.cs que muestre por pantalla los números
+            //pares divisibles por 3 del 1 al 50.
+
+            int num = 50;
+            
+            for(int i = 1; i <= num; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+        }
+    }
+}
